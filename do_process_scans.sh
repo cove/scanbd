4b53a56f909e739af73b41f873a5a96962321edd
Cove Schneider <covecs@gmail.com>
2016-09-03 23:18:00 -0700

    clean up

--- do_process_scans.sh
+++ do_process_scans.sh
@@ -1,10 +1,6 @@
 #!/bin/sh -x
 
 dest=~cove/Dropbox/Scan
-
-success_sound=~cove/scanbd/sounds/Ping.aiff
-fail_sound=~cove/scanbd/sounds/Basso.aiff
-start_sound=~cove/scanbd/sounds/Tink.aiff
 PATH=~cove/scanbd/tools:$PATH
 
 for each2 in /tmp/scan.*; do
@@ -15,20 +11,17 @@ for each2 in /tmp/scan.*; do
 	    each=`basename $each .ppm`
             name=$each
 	    ppmtojpeg $tmpdir/$each.ppm > $tmpdir/$each.jpg
-	    #convert -set option:deskew:auto-crop 100 -deskew 40 \
-	    #	$tmpdir/$each.jpg $tmpdir/u$each.jpg
 	    trimmer -s all -f 20 $tmpdir/$each.jpg $tmpdir/u$each.jpg
-	    #unrotate -f 10 $tmpdir/$each.jpg $tmpdir/u$each.jpg
 	done
 
 	convert $tmpdir/u*.jpg $tmpdir/$name.pdf
-	ocr.sh $tmpdir/$name.pdf $tmpdir/o$name.pdf
+	ocr $tmpdir/$name.pdf $tmpdir/o$name.pdf
 
 	cp $tmpdir/o$name.pdf $dest/$name.pdf
 	chown cove $dest/$name.pdf
 	pdfinfo $dest/$name.pdf
 	if [ $? -eq 0 ]; then
-	 rm -rf $tmpdir
+		rm -rf $tmpdir
 	fi
 done
 
--- do_scan_queue.sh
+++ do_scan_queue.sh
@@ -3,16 +3,10 @@
 tmpdir=/tmp/scan.$$
 name=`date +%F_%H_%M_%S`
 dest=~cove/Dropbox/Scan
-
-success_sound=~cove/scanbd/sounds/Ping.aiff
-fail_sound=~cove/scanbd/sounds/Basso.aiff
-start_sound=~cove/scanbd/sounds/Tink.aiff
 PATH=~cove/scanbd/tools:$PATH
 
 echo "-------------- START SCAN --------------"
 date
-play -q $start_sound 
-play -q $start_sound 
 mkdir -p $tmpdir
 scanadf --mode Color --source "ADF Duplex" --resolution 200 \
 	--df-action Stop \
